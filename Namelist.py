bee29a2d2925a7f48aedbc53aa44c50d632f09b9
BalkanBoy19 <ndrekovic@outlook.de>
2020-08-23 23:37:08 +0200

    fertig mit Datenbankanbindung und Funktionalitäten

--- Namelist.py
+++ Namelist.py
@@ -1,10 +1,15 @@
 # Studentenliste
 import tkinter as tk
+from tkinter import ttk
 import sqlite3
 
 
-def show_entry_fields():
-   print("First Name: %s\nLast Name: %s" % (vorname.get(), name.get()))
+def showAllStudents():
+    zeiger.execute("Select * From Student;")
+    res = zeiger.fetchall()
+    for data in res:
+        print(data[0] + ", " + data[1] + ", " + data[2] + ", " + data[3] + ", " + data[4] + ", " + data[5])
+    print()
 
 def deleteEntries():
     vorname.set("")
@@ -14,10 +19,29 @@ def deleteEntries():
     geschlecht.set("")
     semester.set("")
 
+def deleteStudent():
+    global cb
+    if (len(cb["values"]) > 0) & (cb.get() != ''):
+        student = cb.get()
+        matrNr = student.split(', ')[2]
+        print("---",matrNr,"---")
+        zeiger.execute("DELETE FROM Student Where matrikelnummer = " + matrNr)
+        connection.commit()
+
+        options = list(cb["values"])
+        options.remove(student)
+        cb["values"] = options
+        if len(list(cb["values"])) == 0:
+            cb.set("")
+        else:
+            cb.current(0)
+    else:
+        print("Liste ist leer.")
+
 def dataisValid():
-    if any(letter.isdigit() for letter in vorname.get()):
+    if any(letter.isdigit() for letter in vorname.get()) | (vorname.get() == ''):
         return False
-    if any(letter.isdigit() for letter in nachname.get()):
+    if any(letter.isdigit() for letter in nachname.get()) | (nachname.get() == ''):
         return False
     if any((not digit.isdigit() | (len(matrikelnummer.get()) != 7)) for digit in matrikelnummer.get()):
         return False
@@ -25,19 +49,34 @@ def dataisValid():
         return False
     if geschlecht.get() not in ['m', 'w']:
         return False
-    if any(not digit.isdigit() for digit in semester.get()):
+    if any(not digit.isdigit() for digit in semester.get()) | (semester.get() == ''):
+        return False
+    zeiger.execute("SELECT * FROM Student Where matrikelnummer = " + matrikelnummer.get())
+    connection.commit()
+    res = zeiger.fetchone()
+    if res != None:
+        print("Ein anderer Student ist bereits unter dieser Matrikelnummer eingetragen.")
         return False
     return True
 
+def insertedData():
+    zeiger.execute("SELECT * FROM Student;")
+    studentslist = []
+    for data in zeiger.fetchall():
+        studentslist.append(data[0] + ", " + data[1] + ", " + data[2] + ", " + data[3] + ", " + data[4] + ", " + data[5])
+    return studentslist
+
 def saveData():
     if dataisValid():
-        zeiger = connection.cursor()
-        zeiger.execute("INSERT INTO Student VALUES ('" + vorname.get() + "', '" + nachname.get() + "', '" + matrikelnummer.get() + "', " + alter.get() + ", '" + geschlecht.get() + "', " + semester.get() + ");")
-        zeiger.execute("SELECT * FROM Student;")
-        data = zeiger.fetchone()
-        print(data)
-        print("Daten erfolgreich gespeichert.")
+        #zeiger.execute("DELETE FROM Student;")
+        #connection.commit()
+        zeiger.execute("INSERT INTO Student VALUES ('" + vorname.get() + "', '" + nachname.get() + "', " + matrikelnummer.get() + ", " + alter.get() + ", '" + geschlecht.get() + "', " + semester.get() + ");")
+        connection.commit()     # Committen ist wirklich wichtig, bevor naechste Operation ausgefuehrt wird
         deleteEntries()
+        print("Daten erfolgreich gespeichert.")
+        cb["values"] = insertedData()
+
+
 
 def closing():
     connection.commit()
@@ -49,7 +88,7 @@ if __name__ == '__main__':
 
     connection = sqlite3.connect("/Users/nedimdrekovic/Python/Namelist/list.db")
     zeiger = connection.cursor()
-    zeiger.execute("DROP TABLE IF EXISTS Student;")
+    #zeiger.execute("DROP TABLE IF EXISTS Student;")
     zeiger.execute("""CREATE TABLE IF NOT EXISTS Student (
                         `vorname` TEXT NOT NULL,
                         `name` TEXT NOT NULL,
@@ -81,25 +120,15 @@ if __name__ == '__main__':
 
     tk.Button(master, text='Schließen', command=closing).grid(row=7, column=0, pady=4)
     tk.Button(master, text='Save', command=saveData).grid(row=6, column=0, pady=4)
-    tk.Button(master, text='Anzeigen', command=show_entry_fields).grid(row=7, column=1, pady=4)
+    tk.Button(master, text='Anzeigen', command=showAllStudents).grid(row=7, column=1, pady=4)
+    tk.Button(master, text='Löschen', command=deleteStudent).grid(row=0, column=3, pady=4)
 
+    studentslist = insertedData()
+    #insertData()
+
+    cb = ttk.Combobox(master, state="readonly", values=studentslist)
+    cb.grid(row=0, column=2, pady=5)
+    if len(list(cb["values"])) > 0:
+        cb.current(0)
+    print("Studentslist:",studentslist)
     tk.mainloop()
-"""
-            for letter in vorname.get():
-                if isinstance(letter, int):
-                    return False
-            for letter in nachname.get():
-                if isinstance(letter, int):
-                    return False
-            for digit in matrikelnummer.get():
-                if isinstance(digit, str) || (len(matrikelnummer.get()) != 7):
-                    return False
-            for digit in alter.get():
-                if isinstance(digit, str) || (len(alter.get()) > 3):
-                    return False
-            if geschlecht.get() not in ['m', 'w']:
-                return False
-            for digit in semester.get():
-                if isinstance(digit, str):
-                    return False
-"""
